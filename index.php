f74c4201e0497dddb3e6fd181ce993bb1a0146ab
Ricardo de Oliveira <ricardo.reksystem@gmail.com>
2018-01-18 09:11:49 -0200

    commit via terminal

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    Apenas iniciando os meus testes no GIT
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>teste com GIT</title>
+    </head>
+    <body>
+        <h1>
+        <?php
+            echo 'Olá mundo';
+        ?>
+        </h1>
+    </body>
+</html>
